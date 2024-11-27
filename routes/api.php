bf065e56018d4e7d3ee03be17d56299e5eb69588
nathan <nathancowin01@gmail.com>
2024-11-27 19:08:01 +0000

    more phpcs broken stuff

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware([
-    'auth:sanctum'])->get('/user', function (Request $request) {
+    'auth:sanctum'
+]
+)->get('/user',
+    function (Request
+              $request) {
     return
         $request
             ->user()
