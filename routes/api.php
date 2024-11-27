2489b0d9f9d58749e67722266023906608d00bbb
Nathan-Cowin <Nathan-Cowin@users.noreply.github.com>
2024-11-27 19:17:27 +0000

    Apply automatic changes

--- routes/api.php
+++ routes/api.php
@@ -3,31 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-
 Route::middleware([
-    'auth:sanctum'
+    'auth:sanctum',
 ]
 
-
-
 )->get('/user',
-    function (Request
-              $request) {
-    return
-        $request
-            ->user()
-        ;
-}
-
+    function (Request $request) {
+        return
+            $request
+                ->user();
+    }
 
 );
 
 Route::get('/test', function () {
 
-
-
-    return
-        'test';
+    return 'test';
 });
