71e31b9069e99253bfa20fede04035522ee808a0
Nathan-Cowin <Nathan-Cowin@users.noreply.github.com>
2024-11-27 19:20:35 +0000

    Pint Fixes

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
 Route::middleware([
-
-
-
 
     'auth:sanctum',
 ]
@@ -18,7 +11,6 @@ Route::middleware([
 )->get('/user',
     function (Request $request) {
         return
-
 
             $request
                 ->user();
